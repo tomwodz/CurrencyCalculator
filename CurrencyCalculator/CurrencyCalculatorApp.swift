f284b87c90fd66638711a60d1b20fc0301cadd3d
tomwodz <twodzinski@gmail.com>
2024-06-01 12:53:14 +0200

    Initial Commit

---
+++ CurrencyCalculator/CurrencyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCalculatorApp.swift
+//  CurrencyCalculator
+//
+//  Created by Mini-M1 on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
